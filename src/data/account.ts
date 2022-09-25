c28419df4e07a0268c85a26f48bd82c30a8a4b42
Zac Waite <129032699zw@gmail.com>
2022-09-24 21:17:28 -0400

    Undo long load

--- src/data/account.ts
+++ src/data/account.ts
@@ -4,7 +4,7 @@ import { statusType } from 'types/data';
 import { exampleAccount } from './exampleData';
 
 export const tryGetAuth = async (): Promise<{ status: statusType, auth: AuthContextType }> => {
-	await new Promise((resolve) => setTimeout(resolve, 100000))
+	await new Promise((resolve) => setTimeout(resolve, 1000))
 	return {
 		status: 200,
 		auth: {
