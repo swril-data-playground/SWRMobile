d013752c2c11c1e35f7e2735df2239e4cfa7c791
hargunmujral <hargunmujral@gmail.com>
2022-10-09 21:16:45 -0400

    added 'x' for removing toast

--- src/app/components/toasts/Toast.tsx
+++ src/app/components/toasts/Toast.tsx
@@ -1,8 +1,11 @@
 import { Toast } from '../../../types/toast'
-import { StyleSheet, View, Image } from 'react-native'
+import { StyleSheet, View, Image, ScrollView, Dimensions, Button } from 'react-native'
 import { SWRText } from 'components/SWRText'
 import { images } from "assets/images"
 import { gs } from 'styles/globals'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { SWRButton } from 'components/SWRButton'
+ // import Swipeable from 'react-native-gesture-handler/Swipeable'
 
 const toastMap = {
     'error': images.toast_error_icon,
@@ -11,19 +14,57 @@ const toastMap = {
     'info': images.toast_info_icon,
 }
 
+// const exit = () => {
+
+// }
+
+const {width, height} = Dimensions.get('screen')
+
+
 export const ToastItem = (props: {
 	toast: Toast,
 }) => {
+
+const [dimension, setDimension] = useState(Dimensions.get('window'))
+	const [selectedIndex, setSelectedIndex] = useState(0)
+	const scrollRef = useRef<ScrollView>(null)
+	const intervalId = useRef<NodeJS.Timeout>()
+    const [showToast, setShowToast] = useState(true)
+
+	const onChange = () => {
+		setDimension(Dimensions.get('window'))
+	}
+
+	useEffect(() => {
+		const listener = Dimensions.addEventListener('change', onChange)
+		return () => {
+			listener.remove()
+		};
+	});
+
+	
+	// useEffect(() => {
+		
+	// }, [showToast]);
+
+
     return (
-        <View style={styles.container}>
-            <Image source={toastMap[props.toast.type]} style={styles.image}/>
-            <View style={styles.data}>
-                <SWRText style={gs.h4}>{props.toast.title}</SWRText>
-                {props.toast.details && <SWRText style={gs.h7}>{props.toast.details}</SWRText>}
+
+            showToast && <View style={styles.container}>
+                <Image source={toastMap[props.toast.type]} style={styles.image}/>
+                <View style={styles.data}>
+                    <SWRText style={gs.h4}>{props.toast.title}</SWRText>
+                    {props.toast.details && <SWRText style={gs.h7}>{props.toast.details}</SWRText>}
+                </View>
+                <SWRButton onPress={() => setShowToast(false)} style={{marginLeft: 'auto'}}>
+                    <Image source={images.x} style={styles.xicon}/>
+                </SWRButton>
             </View>
-        </View>
+ 
+
     )
 }
+        
 
 const styles = StyleSheet.create({
     container: {
@@ -44,4 +85,8 @@ const styles = StyleSheet.create({
     },
 	data: {
 	},
+    xicon: {
+        width: 20,
+        height: 20,
+    },
 })
